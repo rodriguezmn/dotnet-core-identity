d085b4cfb22a812ae7a59c034276cfc1c4155b7e
Marlon Rodriguez <mrbox30@gmail.com>
2020-06-01 23:30:30 -0600

    Add project files.

---
+++ Authentication_3.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Authentication_3._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
